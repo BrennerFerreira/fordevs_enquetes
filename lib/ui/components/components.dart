7a71ff681c4bbedbcd978846e47d84f83edd937d
BrennerFerreira <brennercsferreira@gmail.com>
2021-09-18 17:28:49 -0300

    refactor: loading dialog methods to their own file

--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -1,3 +1,4 @@
 export 'app.dart';
 export 'headline_1.dart';
+export 'loading_dialog.dart';
 export 'login_header.dart';
--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -26,32 +26,9 @@ class _LoginPageState extends State<LoginPage> {
         builder: (context) {
           widget.loginPresenter?.isLoadingStream.listen((isLoading) {
             if (isLoading) {
-              showDialog(
-                context: context,
-                barrierDismissible: false,
-                builder: (context) {
-                  return SimpleDialog(
-                    children: [
-                      Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          CircularProgressIndicator.adaptive(),
-                          SizedBox(height: 10),
-                          Text(
-                            "Aguarde...",
-                            textAlign: TextAlign.center,
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
-                },
-              );
+              showLoading(context);
             } else {
-              if (Navigator.canPop(context)) {
-                Navigator.of(context).pop();
-              }
+              hideLoading(context);
             }
           });
 
