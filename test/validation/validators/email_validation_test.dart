4645ded6d55203d71e51cf11452fc3ca7936bc2d
BrennerFerreira <brennercsferreira@gmail.com>
2021-09-19 10:53:05 -0300

    refactor: move duplicated code to setup method

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -2,9 +2,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fordevs_enquetes/validation/validators/validators.dart';
 
 void main() {
-  test('Should return null if email is empty or null', () {
-    final sut = EmailValidation('any field');
+  late EmailValidation sut;
 
+  setUp(() {
+    sut = EmailValidation('any field');
+  });
+
+  test('Should return null if email is empty or null', () {
     expect(sut.validate(value: ''), null);
     expect(sut.validate(), null);
   });
