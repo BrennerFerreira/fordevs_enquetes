0101e2bf7ff93e9a98d580610215539a893ae51e
BrennerFerreira <brennercsferreira@gmail.com>
2021-09-21 13:42:15 -0300

    feat: ensure GetxLoginPresenter emit error if SaveCurrentAccount fails

--- test/presentation/presenters/getx_login_presenter_test.dart
+++ test/presentation/presenters/getx_login_presenter_test.dart
@@ -32,12 +32,19 @@ void main() {
 
   PostExpectation mockAuthenticationCall() => when(authentication.auth(any));
 
+  PostExpectation mockSaveCurrentAccountCall() => when(
+        saveCurrentAccount.save(any),
+      );
+
   void mockAuthentication() => mockAuthenticationCall().thenAnswer(
         (_) async => AccountEntity(token),
       );
 
   void mockAuthenticationError(DomainError error) =>
       mockAuthenticationCall().thenThrow(error);
+
+  void mockSaveCurrentAccountError() =>
+      mockSaveCurrentAccountCall().thenThrow(DomainError.unexpected);
 
   setUp(() {
     validation = MockValidation();
@@ -199,6 +206,23 @@ void main() {
     await sut.auth();
 
     verify(saveCurrentAccount.save(AccountEntity(token))).called(1);
+  });
+
+  test('Should emit UnexpectedError if SaveCurrentAccount fails', () async {
+    mockSaveCurrentAccountError();
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    sut.authErrorStream.listen(expectAsync1(
+      (error) => expect(
+        error,
+        'Algo inesperado aconteceu. Tente novamente em breve',
+      ),
+    ));
+
+    await sut.auth();
   });
 
   test('Should emit correct events on authentication success', () async {
